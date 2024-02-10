4051c22847b7e9ccc1f419878efd7c7f14852a9c
Alaa Fayed <alaafayed810@gmail.com>
2024-02-10 23:01:16 +0200

    test my code

--- models/base_model.py
+++ models/base_model.py
@@ -1,71 +1,50 @@
 #!/usr/bin/python3
+import uuid
 from datetime import datetime
-from uuid import uuid4
 import models
-"""
-Module BaseModel
-Parent of all classes
-"""
+"""importing modules"""
 
 
-class BaseModel():
-    """Base class for Airbnb clone project
-    Methods:
-        __init__(self, *args, **kwargs)
-        __str__(self)
-        __save(self)
-        __repr__(self)
-        to_dict(self)
-    """
-
+class BaseModel:
+    """class that defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-        """
-        Initialize attributes: random uuid, dates created/updated
-        """
+        """A method is useful to do any initialization passing initial values
+        to your object you want to do with your object"""
+        time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
-            for key, val in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "__class__" == key:
-                    pass
-                else:
-                    setattr(self, key, val)
+            del kwargs["__class__"]
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    self.created_at = datetime.strptime(
+                        kwargs["updated_at"], time)
+                elif k == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        kwargs["updated_at"], time)
+            else:
+                setattr(self, k, v)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            models.storage.new()
 
     def __str__(self):
-        """
-        Return string of info about model
-        """
-        return ("[{}] ({}) {}".
-                format(self.__class__.__name__, self.id, self.__dict__))
-
-    def __repr__(self):
-        """
-        returns string representation
-        """
-        return (self.__str__())
+        """method returns a human-readable, or
+        informal, string representation of an object"""
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """
-        Update instance with updated time & save to serialized file
-        """
+        """updates the public instance attribute updated_at with the
+        current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Return dic with string formats of times; add class info to dic
-        """
+        """returns a dictionary containing all keys/values of __dict__
+        of the instance"""
         mydict = self.__dict__.copy()
         new_dict = {**mydict, **{"__class__": self.__class__.__name__},
                     **{"created_at": self.created_at.isoformat()},
-                    **{"updated_at": self.updated_at.isoformat()}}
+                    **{"updated_at": self.updated_at.isoformat}}
         return new_dict
